2802feb32785caa708ce43fca8642ab353b83f1f
Sidharth Gill <sidharthgill19june@gmail.com>
2024-10-16 02:10:22 +0530

    Updates

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
 from FrequencyAbstraction import FourierTransformation
-
+from sklearn.cluster import KMeans
 
 # --------------------------------------------------------------
 # Load data
@@ -140,19 +140,85 @@ ws=int(2800/200)
 
 ds_freq=FreqAbs.abstract_frequency(df_freq,["acc_y"],ws,fs)
 
-#/visualize results
+#visualize results
+subset=df_freq[df_freq["set"]==15]
+subset[["acc_y"]].plot()
+subset[
+    [
+        "acc_y_max_freq",
+        "acc_y_freq_weighted",
+        "acc_y_pse",
+        "acc_y_freq_1.429_Hz_ws_14",
+        "acc_y_freq_2.5_Hz_ws_14",
+    ]
+].plot()
 
+df_freq_list=[]
+for s in df_freq["set"].unique():
+    print(f"Applying Fourier Transformations to set {s}")
+    subset=df_freq[df_freq["set"]==s].reset_index(drop=True).copy()
+    subset=FreqAbs.abstract_frequency(subset,predictor_columns,ws,fs)
+    df_freq_list.append(subset)
 
+df_freq = pd.concat(df_freq_list).set_index("epoch (ms)",drop=True)
 # --------------------------------------------------------------
 # Dealing with overlapping windows
 # --------------------------------------------------------------
-
+df_freq=df_freq.dropna()
+df_freq=df_freq.iloc[::2]
 
 # --------------------------------------------------------------
 # Clustering
 # --------------------------------------------------------------
 
+df_cluster=df_freq.copy()
+
+cluster_columns=["acc_x","acc_y","acc_z"]
+k_values=range(2,10)
+inertias=[]
+
+for k in k_values:
+    subset=df_cluster[cluster_columns]
+    kmeans=KMeans(n_clusters=k,n_init=20,random_state=0)
+    cluster_labels=kmeans.fit_predict(subset)
+    inertias.append(kmeans.inertia_)
+
+plt.figure(figsize=(10,20))
+plt.plot(k_values,inertias)
+plt.xlabel('k')
+plt.ylabel("Sum of Squared distances")
+plt.show()
+
+kmeans=KMeans(n_clusters=5,n_init=20,random_state=0)
+subset=df_cluster[cluster_columns]
+df_cluster["cluster"]=kmeans.fit_predict(subset)
+
+fig=plt.figure(figsize=(15,15))
+ax=fig.add_subplot(projection="3d")
+for c in df_cluster["cluster"].unique():
+    subset=df_cluster[df_cluster["cluster"]==c]
+    ax.scatter(subset["acc_x"],subset["acc_y"],subset["acc_z"],label=c)
+ax.set_xlabel("X-Axis")
+ax.set_ylabel("Y-Axis")
+ax.set_zlabel("Z-Axis")
+plt.legend()
+plt.show()
+
+
+fig=plt.figure(figsize=(15,15))
+ax=fig.add_subplot(projection="3d")
+for l in df_cluster["label"].unique():
+    subset=df_cluster[df_cluster["label"]==l]
+    ax.scatter(subset["acc_x"],subset["acc_y"],subset["acc_z"],label=l)
+ax.set_xlabel("X-Axis")
+ax.set_ylabel("Y-Axis")
+ax.set_zlabel("Z-Axis")
+plt.legend()
+plt.show()
+
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+df_cluster.to_pickle("../../data/interim/03_data_features.pkl")
